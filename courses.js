ef8903df2b00f4c6a67da663bdb1993be7058261
Jusbeersingh Ramo <jusbeersramo@gmail.com>
2024-12-02 17:54:55 +0400

    Add API and readme

---
+++ courses.js
@@ -0,0 +1,12 @@
+let courses = [
+    {"id": 1, "name": "Mathematics", "location": "Mauritius", "Price": 500, "Space": 5, "image": "maths.webp"},
+    {"id": 2, "name": "English", "location": "Mauritius", "Price": 500, "Space": 5, "image": "english.webp"},
+    {"id": 3, "name": "French", "location": "Mauritius", "Price": 500, "Space": 5, "image": "french.webp"},
+    {"id": 4, "name": "Design & Tech","location": "Mauritius", "Price": 450, "Space": 5, "image": "design.webp"},
+    {"id": 5, "name": "Chemistry", "location": "Mauritius", "Price": 1500, "Space": 5, "image": "chemistry.webp"},
+    {"id": 6, "name": "Music", "location": "Mauritius", "Price": 400, "Space": 5, "image": "music.webp"},
+    {"id": 7, "name": "Computer Science", "location": "Mauritius", "Price": 600, "Space": 5, "image": "computer.webp"},
+    {"id": 8, "name": "Kendo", "location": "Mauritius", "Price": 1000, "Space": 5, "image": "kendo.webp"},
+    {"id": 9, "name": "Cooking", "location": "Mauritius", "Price": 800, "Space": 5, "image": "cooking.webp"},
+    {"id": 10, "name": "Volleyball", "location": "Mauritius", "Price": 1000, "Space": 5, "image": "volley.webp"}
+]
